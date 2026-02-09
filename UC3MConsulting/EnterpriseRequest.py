1ecc70b41a53176d9554d4a084fc1b0fd6ad1239
adriannaalvarez <adriannadalvarez@gmail.com>
2026-02-09 17:32:13 +0100

    fixed attribute naming convention

--- UC3MConsulting/EnterpriseRequest.py
+++ UC3MConsulting/EnterpriseRequest.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 class EnterpriseRequest:
     def __init__(self, Cif,phOnE, ENAME):
-        self.__EnterpriseNAME = ENAME
-        self.__cIF = Cif
-        self.__pHONe = phOnE
+        self.enterpriseName = ENAME
+        self.cIf = Cif
+        self.phone = phOnE
         justnow = datetime.utcnow()
         self.__timeStamp = datetime.timestamp(justnow)
 
@@ -14,22 +14,22 @@ class EnterpriseRequest:
         return "Enterprise:" + json.dumps(self.__dict__)
 
     @property
-    def ENTERPRISE_CIF(self):
-        return self.__cIF
-    @ENTERPRISE_CIF.setter
-    def ENTERPRISE_CIF(self, value):
-        self.__cIF = value
+    def enterpriseCIf(self):
+        return self.cIf
+    @enterpriseCIf.setter
+    def enterpriseCIf(self, value):
+        self.cIf = value
 
     @property
-    def PHONE_NUMBER(self):
-        return self.__pHONe
-    @PHONE_NUMBER.setter
-    def PHONE_NUMBER(self, value):
-        self.__pHONe = value
+    def phoneNumber(self):
+        return self.phone
+    @phoneNumber.setter
+    def phoneNumber(self, value):
+        self.phone = value
 
     @property
-    def ENTerprise_Name(self):
-        return self.__EnterpriseNAME
-    @ENTerprise_Name.setter
-    def ENTerprise_Name(self, value):
-        self.__EnterpriseNAME = value
+    def enterpriseName(self):
+        return self.enterpriseName
+    @enterpriseName.setter
+    def enterpriseName(self, value):
+        self.enterpriseName = value
